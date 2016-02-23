f256abec7bdb4471e74a4ac7e17f37d327c69ec3
Felipe Cabral <felipehistory@gmail.com>
2016-02-23 17:01:40 -0300

    add namespace and welcome text

--- Theme.php
+++ Theme.php
@@ -1,5 +1,5 @@
 <?php
-namespace TemplateV1;
+namespace mapasculturaisThemeBH;
 use MapasCulturais\Themes\BaseV1;
 use MapasCulturais\App;
 
@@ -14,25 +14,36 @@ class Theme extends BaseV1\Theme{
         $url_search_projects = $self->searchProjectsUrl;
 
         return [
-//            'site: name' => App::i()->config['app.siteName'],
-//            'site: description' => App::i()->config['app.siteDescription'],
-//            'site: in the region' => 'na região',
-//            'site: of the region' => 'da região',
-//            'site: owner' => 'Secretaria',
-//            'site: by the site owner' => 'pela Secretaria',
-//
-//            'home: title' => "Bem-vind@!",
-//            'home: abbreviation' => "MC",
-//            'home: colabore' => "Colabore com o Mapas Culturais",
-//            'home: welcome' => "O Mapas Culturais é uma plataforma livre, gratuita e colaborativa de mapeamento cultural.",
-//            'home: events' => "Você pode pesquisar eventos culturais nos campos de busca combinada. Como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente.",
-//            'home: agents' => "Você pode colaborar na gestão da cultura com suas próprias informações, preenchendo seu perfil de agente cultural. Neste espaço, estão registrados artistas, gestores e produtores; uma rede de atores envolvidos na cena cultural da região. Você pode cadastrar um ou mais agentes (grupos, coletivos, bandas instituições, empresas, etc.), além de associar ao seu perfil eventos e espaços culturais com divulgação gratuita.",
-//            'home: spaces' => "Procure por espaços culturais incluídos na plataforma, acessando os campos de busca combinada que ajudam na precisão de sua pesquisa. Cadastre também os espaços onde desenvolve suas atividades artísticas e culturais.",
-//            'home: projects' => "Reúne projetos culturais ou agrupa eventos de todos os tipos. Neste espaço, você encontra leis de fomento, mostras, convocatórias e editais criados, além de diversas iniciativas cadastradas pelos usuários da plataforma. Cadastre-se e divulgue seus projetos.",
-//            'home: home_devs' => 'Existem algumas maneiras de desenvolvedores interagirem com o Mapas Culturais. A primeira é através da nossa <a href="https://github.com/hacklabr/mapasculturais/blob/master/doc/api.md" target="_blank">API</a>. Com ela você pode acessar os dados públicos no nosso banco de dados e utilizá-los para desenvolver aplicações externas. Além disso, o Mapas Culturais é construído a partir do sofware livre <a href="http://institutotim.org.br/project/mapas-culturais/" target="_blank">Mapas Culturais</a>, criado em parceria com o <a href="http://institutotim.org.br" target="_blank">Instituto TIM</a>, e você pode contribuir para o seu desenvolvimento através do <a href="https://github.com/hacklabr/mapasculturais/" target="_blank">GitHub</a>.',
-//
-//            'search: verified results' => 'Resultados Verificados',
-//            'search: verified' => "Verificados"
+            'site: name' => App::i()->config['app.siteName'],
+            'site: description' => App::i()->config['app.siteDescription'],
+            'site: in the region' => 'na região',
+            'site: of the region' => 'da região',
+            'site: owner' => 'Secretaria',
+            'site: by the site owner' => 'pela Secretaria',
+
+            'home: title' => "Bem-vind@!",
+            'home: abbreviation' => "MC",
+            'home: colabore' => "Colabore com o Mapas Culturais",
+            'home: welcome' => "A Fundação Municipal de Cultura disponibiliza para a comunidade belo-horizontina e todo o Brasil, 
+				informações sobre o cenário cultural da cidade de Belo Horizonte no Mapa Cultura BH. O Mapa Cultura 
+				BH é uma plataforma de software livre e gratuita para o mapeamento colaborativo de agentes, espaços, 
+				eventos e projetos culturais da cidade. A plataforma é alimentada pelos próprios participantes e está 
+				alinhada ao Sistema Nacional de Informações e Indicadores Culturais (SNIIC) do Ministério da Cultura, 
+				permitindo a integração e a visualização das informações culturais em âmbito nacional. Ficou mais fácil 
+				se programar para conhecer as opções culturais que a cidade oferece: shows musicais, espetáculos de 
+				teatro e dança, cinema, festas e manifestações tradicionais e populares, saraus, entre outras. Além de 
+				acompanhar a agenda de eventos, você também pode colaborar na gestão da cultura da cidade: basta criar 
+				seu perfil de agente cultural. A partir deste cadastro, você pode participar dos editais de fomento e 
+				divulgar seus eventos, espaços ou projetos.",
+
+            'home: events' => "Você pode pesquisar eventos culturais nos campos de busca combinada. Como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente.",
+            'home: agents' => "Você pode colaborar na gestão da cultura com suas próprias informações, preenchendo seu perfil de agente cultural. Neste espaço, estão registrados artistas, gestores e produtores; uma rede de atores envolvidos na cena cultural da região. Você pode cadastrar um ou mais agentes (grupos, coletivos, bandas instituições, empresas, etc.), além de associar ao seu perfil eventos e espaços culturais com divulgação gratuita.",
+            'home: spaces' => "Procure por espaços culturais incluídos na plataforma, acessando os campos de busca combinada que ajudam na precisão de sua pesquisa. Cadastre também os espaços onde desenvolve suas atividades artísticas e culturais.",
+            'home: projects' => "Reúne projetos culturais ou agrupa eventos de todos os tipos. Neste espaço, você encontra leis de fomento, mostras, convocatórias e editais criados, além de diversas iniciativas cadastradas pelos usuários da plataforma. Cadastre-se e divulgue seus projetos.",
+            'home: home_devs' => 'Existem algumas maneiras de desenvolvedores interagirem com o Mapas Culturais. A primeira é através da nossa <a href="https://github.com/hacklabr/mapasculturais/blob/master/doc/api.md" target="_blank">API</a>. Com ela você pode acessar os dados públicos no nosso banco de dados e utilizá-los para desenvolver aplicações externas. Além disso, o Mapas Culturais é construído a partir do sofware livre <a href="http://institutotim.org.br/project/mapas-culturais/" target="_blank">Mapas Culturais</a>, criado em parceria com o <a href="http://institutotim.org.br" target="_blank">Instituto TIM</a>, e você pode contribuir para o seu desenvolvimento através do <a href="https://github.com/hacklabr/mapasculturais/" target="_blank">GitHub</a>.',
+
+            'search: verified results' => 'Resultados do Mapeamento Cultural BH',
+            'search: verified' => "Verificados"
         ];
     }
 
