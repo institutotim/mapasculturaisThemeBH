8646a4ddbccb89bed8d775812eee9f7e2da90457
robertkapa <felipehistory@gmail.com>
2016-04-11 13:54:17 -0300

    add fundação municipal de cultural as verified results

--- Theme.php
+++ Theme.php
@@ -22,7 +22,7 @@ class Theme extends BaseV1\Theme{
             'site: by the site owner' => 'pela Secretaria',
 
             'home: title' => "Bem-vind@!",
-            'home: abbreviation' => "MC",
+            'home: abbreviation' => "Fundação Municipal de Cultura",
             'home: colabore' => "Colabore com o Mapas Culturais",
             'home: welcome' => "A Fundação Municipal de Cultura disponibiliza para a comunidade belo-horizontina e todo o Brasil, 
 				informações sobre o cenário cultural da cidade de Belo Horizonte no Mapa Cultura BH. O Mapa Cultura 
@@ -43,7 +43,7 @@ class Theme extends BaseV1\Theme{
             'home: home_devs' => 'Existem algumas maneiras de desenvolvedores interagirem com o Mapas Culturais. A primeira é através da nossa <a href="https://github.com/hacklabr/mapasculturais/blob/master/doc/api.md" target="_blank">API</a>. Com ela você pode acessar os dados públicos no nosso banco de dados e utilizá-los para desenvolver aplicações externas. Além disso, o Mapas Culturais é construído a partir do sofware livre <a href="http://institutotim.org.br/project/mapas-culturais/" target="_blank">Mapas Culturais</a>, criado em parceria com o <a href="http://institutotim.org.br" target="_blank">Instituto TIM</a>, e você pode contribuir para o seu desenvolvimento através do <a href="https://github.com/hacklabr/mapasculturais/" target="_blank">GitHub</a>.',
 
             'search: verified results' => 'Resultados do Mapeamento Cultural BH',
-            'search: verified' => "Verificados"
+            'search: verified' => "Verificados pela Fundação Municipal de Cultura"
         ];
     }
 
