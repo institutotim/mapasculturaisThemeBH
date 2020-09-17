5b1f96c5c859b78a7956865d1804c8cd6ccabf43
dvdti <astin.fmc@pbh.gov.br>
2020-09-17 11:49:03 -0300

    copativel com versão 4.17 mapa ceará

--- layouts/parts/header-main-nav.php
+++ layouts/parts/header-main-nav.php
@@ -24,65 +24,92 @@
 <?php $fake_options = ob_get_clean(); endif; ?>
 
 <nav id="main-nav" class="clearfix">
-    <ul class="menu entities-menu clearfix">
+   <ul class="menu entities-menu clearfix">
         <?php if($app->isEnabled('events')): ?>
             <?php $this->applyTemplateHook('nav.main.events','before'); ?>
-            <li id="entities-menu-event" ng-class="{'active':data.global.filterEntity === 'event'}" ng-click="tabClick('event')">
-                <a href="<?php if ($this->controller->action !== 'search') echo $app->createUrl('busca') . '##(global:(enabled:(event:!t),filterEntity:event))'; ?>">
+            <li id="entities-menu-event"
+                ng-class="{'active':data.global.filterEntity === 'event',
+                           'current-entity-parent':'<?php echo $this->controller->id;?>' == 'event'}"
+                ng-click="tabClick('event')">
+                <a href="<?php if ($this->controller->action !== 'search') echo $app->createUrl('site', 'search') . '##(global:(enabled:(event:!t),filterEntity:event))'; ?>">
                     <div class="icon icon-event"></div>
-                    <div class="menu-item-label">Eventos</div>
+                    <div class="menu-item-label"><?php $this->dict('entities: Events') ?></div>
                 </a>
             </li>
             <?php $this->applyTemplateHook('nav.main.events','after'); ?>
         <?php endif; ?>
-            
+
         <?php if($app->isEnabled('spaces')): ?>
             <?php $this->applyTemplateHook('nav.main.spaces','before'); ?>
-            <li id="entities-menu-space" ng-class="{'active':data.global.filterEntity === 'space'}" ng-click="tabClick('space')">
-                <a href="<?php if ($this->controller->action !== 'search') echo $app->createUrl('busca') . '##(global:(enabled:(space:!t),filterEntity:space))'; ?>">
+            <li id="entities-menu-space"
+                ng-class="{'active':data.global.filterEntity === 'space',
+                           'current-entity-parent':'<?php echo $this->controller->id;?>' == 'space'}"
+                ng-click="tabClick('space')">
+                <a href="<?php if ($this->controller->action !== 'search') echo $app->createUrl('site', 'search') . '##(global:(enabled:(space:!t),filterEntity:space))'; ?>">
                     <div class="icon icon-space"></div>
                     <div class="menu-item-label"><?php $this->dict('entities: Spaces') ?></div>
                 </a>
             </li>
             <?php $this->applyTemplateHook('nav.main.spaces','after'); ?>
         <?php endif; ?>
-        
+
         <?php if($app->isEnabled('agents')): ?>
             <?php $this->applyTemplateHook('nav.main.agents','before'); ?>
-            <li id="entities-menu-agent" ng-class="{'active':data.global.filterEntity === 'agent'}" ng-click="tabClick('agent')">
-                <a href="<?php if ($this->controller->action !== 'search') echo $app->createUrl('busca') . '##(global:(enabled:(agent:!t),filterEntity:agent))'; ?>">
+            <li id="entities-menu-agent"
+                ng-class="{'active':data.global.filterEntity === 'agent',
+                           'current-entity-parent':'<?php echo $this->controller->id;?>' == 'agent'}"
+                ng-click="tabClick('agent')">
+                <a href="<?php if ($this->controller->action !== 'search') echo $app->createUrl('site', 'search') . '##(global:(enabled:(agent:!t),filterEntity:agent))'; ?>">
                     <div class="icon icon-agent"></div>
-                    <div class="menu-item-label">Agentes</div>
+                    <div class="menu-item-label"><?php $this->dict('entities: Agents') ?></div>
                 </a>
             </li>
             <?php $this->applyTemplateHook('nav.main.agents','after'); ?>
         <?php endif; ?>
-            
+
         <?php if($app->isEnabled('projects')): ?>
             <?php $this->applyTemplateHook('nav.main.projects','before'); ?>
-            <li id="entities-menu-project"  ng-class="{'active':data.global.filterEntity === 'project'}" ng-click="tabClick('project')">
-                <a href="<?php if ($this->controller->action !== 'search') echo $app->createUrl('busca') . '##(global:(enabled:(project:!t),filterEntity:project,viewMode:list))'; ?>">
+            <li id="entities-menu-project"
+                ng-class="{'active':data.global.filterEntity === 'project',
+                           'current-entity-parent':'<?php echo $this->controller->id;?>' == 'project'}"
+                ng-click="tabClick('project')">
+                <a href="<?php if ($this->controller->action !== 'search') echo $app->createUrl('site', 'search') . '##(global:(enabled:(project:!t),filterEntity:project,viewMode:list))'; ?>">
                     <div class="icon icon-project"></div>
-                    <div class="menu-item-label">Projetos</div>
+                    <div class="menu-item-label"><?php $this->dict('entities: Projects') ?></div>
                 </a>
             </li>
             <?php $this->applyTemplateHook('nav.main.projects','after'); ?>
         <?php endif; ?>
+            
+        <?php if($app->isEnabled('opportunities')): ?>
+            <?php $this->applyTemplateHook('nav.main.opportunities','before'); ?>
+            <li id="entities-menu-opportunity"
+                ng-class="{'active':data.global.filterEntity === 'opportunity',
+                           'current-entity-parent':'<?php echo $this->controller->id;?>' == 'opportunity'}"
+                ng-click="tabClick('opportunity')">
+                <a href="<?php if ($this->controller->action !== 'search') echo $app->createUrl('site', 'search') . '##(global:(enabled:(opportunity:!t),filterEntity:opportunity,viewMode:list))'; ?>">
+                    <div class="icon icon-opportunity"></div>
+                    <div class="menu-item-label"><?php $this->dict('entities: Opportunities') ?></div>
+                </a>
+            </li>
+            <?php $this->applyTemplateHook('nav.main.opportunities','after'); ?>
+        <?php endif; ?>
+
     </ul>
     <!--.menu.entities-menu-->
-    <ul class="menu session-menu clearfix">
+     <ul class="menu session-menu clearfix">
         <?php if ($app->auth->isUserAuthenticated()): ?>
             <?php $this->applyTemplateHook('nav.main.notifications','before'); ?>
-            <li class="notifications" ng-controller="NotificationController" ng-hide="data.length == 0">
+            <li class="notifications" ng-controller="NotificationController" style="display:none" ng-class="{'visible': data.length > 0}">
                 <a class="js-submenu-toggle" data-submenu-target="$(this).parent().find('.submenu')">
                     <div class="icon icon-notifications"></div>
-                    <div class="menu-item-label">Notificações</div>
+                    <div class="menu-item-label"><?php \MapasCulturais\i::_e("Notificações");?></div>
                 </a>
                 <ul class="submenu hidden">
                     <li>
                         <div class="clearfix">
-                            <h6 class="alignleft">Notificações</h6>
-                            <a href="#" style="display:none" class="staging-hidden hltip icon icon-check_alt" title="Marcar todas como lidas"></a>
+                            <h6 class="alignleft"><?php \MapasCulturais\i::_e("Notificações");?></h6>
+                            <a href="#" style="display:none" class="staging-hidden hltip icon icon-check_alt" title="<?php \MapasCulturais\i::esc_attr_e("Marcar todas como lidas");?>"></a>
                         </div>
                         <ul>
                             <li ng-repeat="notification in data" on-last-repeat="adjustScroll();">
@@ -90,111 +117,44 @@
                                     <span ng-bind-html="notification.message"></span>
                                     <br>
 
-                                    <a ng-if="notification.request.permissionTo.approve" class="btn btn-small btn-success" ng-click="approve(notification.id)">aceitar</a>
+                                    <a ng-if="notification.request.permissionTo.approve" class="btn btn-small btn-success" ng-click="approve(notification.id)"><?php \MapasCulturais\i::_e("aceitar");?></a>
 
                                     <span ng-if="notification.request.permissionTo.reject">
-                                        <span ng-if="notification.request.requesterUser.id === MapasCulturais.userId">
-                                            <a class="btn btn-small btn-default" ng-click="reject(notification.id)">cancelar</a>
-                                            <a class="btn btn-small btn-success" ng-click="delete(notification.id)">ok</a>
+                                        <span ng-if="notification.request.requesterUser === MapasCulturais.userId">
+                                            <a class="btn btn-small btn-default" ng-click="reject(notification.id)"><?php \MapasCulturais\i::_e("cancelar");?></a>
+                                            <a class="btn btn-small btn-success" ng-click="delete(notification.id)"><?php \MapasCulturais\i::_e("ok");?></a>
                                         </span>
-                                        <span ng-if="notification.request.requesterUser.id !== MapasCulturais.userId">
-                                            <a class="btn btn-small btn-danger" ng-click="reject(notification.id)">rejeitar</a>
+                                        <span ng-if="notification.request.requesterUser !== MapasCulturais.userId">
+                                            <a class="btn btn-small btn-danger" ng-click="reject(notification.id)"><?php \MapasCulturais\i::_e("rejeitar");?></a>
                                         </span>
                                     </span>
 
-                                    <span ng-if="!notification.isRequest">
-                                        <a class="btn btn-small btn-success" ng-click="delete(notification.id)">ok</a>
+                                    <span ng-if="!notification.request">
+                                        <a class="btn btn-small btn-success" ng-click="delete(notification.id)"><?php \MapasCulturais\i::_e("ok");?></a>
                                     </span>
 
                                 </p>
                             </li>
                         </ul>
                         <a href="<?php echo $app->createUrl('panel'); ?>">
-                            Ver todas atividades
+                            <?php \MapasCulturais\i::_e("Ver todas atividades");?>
                         </a>
                     </li>
                 </ul>
                 <!--.submenu-->
             </li>
             <!--.notifications-->
-            <?php $this->applyTemplateHook('nav.main.notifications','after'); ?>
+
+ <?php $this->applyTemplateHook('nav.main.notifications','after'); ?>
+
+            <?php $this->part('nav-main-user') ?>
             
-            <?php $this->applyTemplateHook('nav.main.user','before'); ?>
-            <li class="user">
-                <a href="#" class="js-submenu-toggle" data-submenu-target="$(this).parent().find('.submenu')">
-                    <div class="avatar">
-                        <?php if ($app->user->profile->avatar): ?>
-                            <img src="<?php echo $app->user->profile->avatar->transform('avatarSmall')->url; ?>" />
-                        <?php else: ?>
-                            <img src="<?php $this->asset('img/avatar--agent.png'); ?>" />
-                        <?php endif; ?>
-                    </div>
-                </a>
-                <ul class="submenu hidden">
-                    <li>
-                        <a href="<?php echo $app->createUrl('panel'); ?>">Painel</a>
-                    </li>
-                    <?php if($app->isEnabled('events')): ?>
-                        <?php $this->applyTemplateHook('nav.dropdown.events','before'); ?>
-                        <li>
-                            <a href="<?php echo $app->createUrl('panel', 'events') ?>">Meus Eventos</a>
-                            <a class="add" href="<?php echo $app->createUrl('event', 'create') ?>" ></a>
-                        </li>
-                        <?php $this->applyTemplateHook('nav.dropdown.events','after'); ?>
-                    <?php endif; ?>
-                        
-                    <?php if($app->isEnabled('agents')): ?>
-                        <?php $this->applyTemplateHook('nav.dropdown.agents','before'); ?>
-                        <li>
-                            <a href="<?php echo $app->createUrl('panel', 'agents') ?>">Meus Agentes</a>
-                            <a class="add" href="<?php echo $app->createUrl('agent', 'create') ?>"></a>
-                        </li>
-                        <?php $this->applyTemplateHook('nav.dropdown.agents','after'); ?>
-                    <?php endif; ?>
-                        
-                    <?php if($app->isEnabled('spaces')): ?>
-                        <?php $this->applyTemplateHook('nav.dropdown.spaces','before'); ?>
-                        <li>
-                            <a href="<?php echo $app->createUrl('panel', 'spaces') ?>"><?php $this->dict('entities: My Spaces') ?></a>
-                            <a class="add"href="<?php echo $app->createUrl('space', 'create') ?>"></a>
-                        </li>
-                        <?php $this->applyTemplateHook('nav.dropdown.spaces','after'); ?>
-                    <?php endif; ?>
-                        
-                    <?php if($app->isEnabled('projects')): ?>
-                        <?php $this->applyTemplateHook('nav.dropdown.projects','before'); ?>
-                        <li>
-                            <a href="<?php echo $app->createUrl('panel', 'projects') ?>">Meus Projetos</a>
-                            <a class="add" href="<?php echo $app->createUrl('project', 'create') ?>"></a>
-                        </li>
-                        <?php $this->applyTemplateHook('nav.dropdown.projects','after'); ?>
-                        
-                        <?php $this->applyTemplateHook('nav.dropdown.registrations','before'); ?>
-                        <li>
-                            <a href="<?php echo $app->createUrl('panel', 'registrations') ?>">Minhas Inscrições</a>
-                        </li>
-                        <?php $this->applyTemplateHook('nav.dropdown.registrations','after'); ?>
-                    <?php endif; ?>
-                        
-                    <li class="row"></li>
-                    <!--<li><a href="#">Ajuda</a></li>-->
-                    <li>
-                        <?php if($app->getConfig('auth.provider') === 'Fake'): ?>
-                            <a href="<?php echo $app->createUrl('auth'); ?>">Trocar Usuário</a>
-                            <?php if(!empty($fake_options)) echo $fake_options; ?>
-                        <?php endif; ?>
-			<a href="<?php echo $app->createUrl('auth', 'logout'); ?>" onclick="return logoutForce()">Sair</a>
-		    </li>
-                </ul>
-            </li>
-            <!--.user-->
-            <?php $this->applyTemplateHook('nav.main.user','after'); ?>
         <?php else: ?>
             <?php $this->applyTemplateHook('nav.main.login','before'); ?>
             <li class="login">
-                <a href="<?php echo $app->createUrl('panel') ?>">
+                <a ng-click="setRedirectUrl()" <?php echo $this->getLoginLinkAttributes() ?> >
                     <div class="icon icon-login"></div>
-                    <div class="menu-item-label">Entrar</div>
+                    <div class="menu-item-label"><?php \MapasCulturais\i::_e("Entrar");?></div>
                 </a>
                 <?php if(!empty($fake_options)): ?>
                     <ul class="submenu" style="margin: 2px 0 0 -12px"><li><?php echo str_ireplace("Login\n        </a>", 'Login</a> |', $fake_options) ?></li></ul>
@@ -206,10 +166,9 @@
     </ul>
     <!--.menu.session-menu-->
 </nav>
-
 <script language="javascript" type="text/javascript">
 function logoutForce() {
-    window.open('http://id.cultura.gov.br/logout', '_blank');
+    window.open('https://id.cultura.gov.br/logout', '_blank');
     var oldURL = document.referrer;
     alert("Logout efetuado com sucesso!");
     location.reload();
---
+++ views/opportunity/report.php
@@ -0,0 +1,168 @@
+<?php
+use MapasCulturais\Entities\Registration as R;
+use MapasCulturais\Entities\Agent;
+use MapasCulturais\i;
+
+function echoStatus($registration) {
+    switch ($registration->status){
+        case R::STATUS_APPROVED:
+            i::_e('selecionada');
+            break;
+
+        case R::STATUS_NOTAPPROVED:
+            i::_e('não selecionada');
+            break;
+
+        case R::STATUS_WAITLIST:
+            i::_e('suplente');
+            break;
+
+        case R::STATUS_INVALID:
+            i::_e('inválida');
+            break;
+
+        case R::STATUS_SENT:
+            i::_e('pendente');
+            break;
+    }
+}
+
+function showIfField($hasField, $showField) {
+    if($hasField)
+        echo "<th>" . $showField . "</th>";
+}
+
+$_properties = $app->config['registration.propertiesToExport'];
+$custom_fields = [];
+foreach($entity->registrationFieldConfigurations as $field) :
+    $custom_fields[$field->displayOrder] = [
+        'title' => $field->title,
+        'field_name' => $field->getFieldName(),
+		'type' => $field->fieldType
+    ];
+endforeach;
+
+ksort($custom_fields);
+?>
+<style>
+    tbody td, table th{
+        text-align: left !important;
+        border:1px solid black !important;
+    }
+</style>
+
+<table>
+    <thead>
+        <tr>
+            <th> <?php i::_e("Número") ?> </th>
+			<th> <?php i::_e("Link") ?> </th>
+
+            <?php showIfField($entity->projectName, i::__("Nome do projeto")); ?>
+
+            <th> <?php i::_e("Avaliação") ?> </th>
+            <th><?php i::_e("Status") ?></th>
+            <th><?php i::_e("Inscrição - Data de envio") ?></th>
+            <th><?php i::_e("Inscrição - Hora de envio") ?></th>
+            <?php showIfField($entity->registrationCategories, $entity->registrationCategTitle); ?>
+
+            <?php
+            foreach($custom_fields as $field)
+                echo "<th>" . $field['title'] . "</th>";
+            ?>
+
+            <th><?php i::_e('Anexos') ?></th>
+            <?php foreach($entity->getUsedAgentRelations() as $def): ?>
+                <th><?php echo $def->label; ?></th>
+                
+                <th><?php echo $def->label; ?> - <?php i::_e("Área de Atuação") ?></th>
+                
+                <?php foreach($_properties as $prop): if($prop === 'name') continue; ?>
+                    <th><?php echo $def->label; ?> - <?php echo Agent::getPropertyLabel($prop); ?></th>
+                <?php endforeach; ?>
+            <?php endforeach; ?>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($entity->sentRegistrations as $r): ?>
+            <tr>
+                <td><a href="<?php echo $r->singleUrl; ?>" target="_blank"><?php echo $r->number; ?></a></td>
+				<td><a href="<?php echo $r->singleUrl; ?>" target="_blank"><?php echo $r->singleUrl; ?></a></td>
+                <?php if($entity->projectName): ?>
+                <td><?php echo $r->projectName ?></td>
+                <?php endif; ?>
+                <td><?php echo $r->getEvaluationResultString(); ?></td>
+                <td><?php echoStatus($r); ?></td>
+                <?php $dataHoraEnvio = $r->sentTimestamp; ?>
+                <td><?php echo (!is_null($dataHoraEnvio))? $dataHoraEnvio->format('d-m-Y') : '';?></td>
+                <td><?php echo (!is_null($dataHoraEnvio))? $dataHoraEnvio->format('H:i:s'): '';?></td>
+
+                <?php if($entity->registrationCategories): ?>
+                <td><?php echo $r->category ?></td>
+                <?php endif; ?>
+
+                <?php
+                foreach($custom_fields as $field) {
+                    $_field_val = (isset($field["field_name"])) ? $r->{$field["field_name"]} : "";
+                    echo "<td>";
+										
+					if ($field['type'] === 'cpf'){
+						echo preg_replace("/(\d{3})(\d{3})(\d{3})(\d{2})/", "\$1.\$2.\$3-\$4", $_field_val);
+					}else if ($field['type'] === 'cnpj'){
+						echo preg_replace("/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/", "\$1.\$2.\$3/\$4-\$5", $_field_val);
+					}else{
+						echo (is_array($_field_val)) ? implode(", ", $_field_val) : $_field_val;
+					}
+                    echo "</td>";                
+                }
+                ?>
+
+                <td>
+                    <?php if(key_exists('zipArchive', $r->files)): ?>
+                        <a href="<?php echo $r->files['zipArchive']->url; ?>"><?php i::_e("zip");?></a>
+                     <?php endif; ?>
+                </td>
+
+                <?php
+                foreach($r->_getDefinitionsWithAgents() as $def):
+                    if($def->use == 'dontUse') continue;
+                    $agent = $def->agent;
+                    $agentsData = $r->agentsData;
+                    $agentsDataGroup = [];
+                    if(!empty($agent) && !empty($agentsData)){
+                        $agentsDataGroup = (isset($r->agentsData[$def->agentRelationGroupName])) ? $r->agentsData[$def->agentRelationGroupName] : [];
+                    }
+                ?>
+
+                    <?php if($agent): ?>
+                        <td><a href="<?php echo $agent->singleUrl; ?>" target="_blank"><?php echo (isset($agentsDataGroup['name']))? $agentsDataGroup['name'] : 'Agente';?></a></td>
+                        
+                        <td><?php echo implode(', ', $agent->terms['area']); ?></td>
+
+                        <?php
+                        foreach($_properties as $prop):
+                            if($prop === 'name') continue;
+                            $val = isset($agentsDataGroup[$prop]) ? $agentsDataGroup[$prop] : '';
+                        ?>
+                        <td>
+                            <?php
+                                if ($prop === 'location')
+                                    echo (isset($val['latitude']) && isset($val['longitude'])) ? "{$val['latitude']},{$val['longitude']}" : '';
+                                else
+                                    echo $val;
+                            ?>
+                        </td>
+
+                        <?php endforeach; ?>
+
+                    <?php else: ?>
+                        <?php 
+                            // total de propriedades + 1 coluna que corresponde a $agent->terms['area']
+                            echo str_repeat('<td></td>', count($_properties)+1) 
+                        ?>
+                    <?php endif; ?>
+
+                <?php endforeach ?>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
